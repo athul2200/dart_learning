635de2734ef3fada3a9e7f42d2f16f1b22a707ca
Athul Sudheer <athulsudheer2007@gmail.com>
2026-05-12 23:18:05 +0530

    some exercises done

---
+++ dart_exercise/factorial.dart
@@ -0,0 +1,11 @@
+void main(){
+  int fact=1;
+  int n=5;
+  while(n>0)
+    {
+      fact=fact*n;
+      n=n-1;
+
+    }
+    print("the factorial is $fact");
+}
---
+++ dart_exercise/swap_two.dart
@@ -0,0 +1,10 @@
+void main(){
+  int a=3;
+  int b=2;
+
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  print("the A $a");
+  print("$b");
+}
